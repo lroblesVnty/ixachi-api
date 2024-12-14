c7fed8adb439c6669ed6e113b91353e89d53d0f5
Luis <luis.rs2602@outlook.com>
2024-12-13 20:56:36 -0600

    validation of setLevantamiento with form request

--- app/Http/Controllers/LevantamientoController.php
+++ app/Http/Controllers/LevantamientoController.php
@@ -6,69 +6,31 @@ use App\Http\Controllers\Controller;
 use App\Models\Levantamiento;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
+use App\Http\Requests\StoreLevantamientoRequest;
 
 class LevantamientoController extends Controller{
 
-    public function store(Request $request){
-        $rules= [
-            'fechaLev' => 'required|date_format:d/m/Y',
-            'observaciones' => 'nullable|string',
-            'idPermiso' => 'required|unique:geslevantamientos,idPermiso|integer|max_digits:5',
-            'idPersonal' => 'required|integer|exists:catpersonal,IdPersonal|max_digits:5',
-            //'imgUrl' => 'required|unique:geslevantamientos,idPermiso',
-            //'archivos',
-            'finiquito' => 'required|unique:geslevantamientos,numFiniquito|numeric|max_digits:5',
-            //TODO agregar los campos del detalle levantamiento
-            'detalleLev'=>'required|array',
-            'detalleLev.*.tipoLinea'=>'required|string|max:2|in:R,A,O',
-            'detalleLev.*.linea'=>'required|integer|max_digits:10|exists:gesconfiguracionlineas,linea',
-            'detalleLev.*.estacaIni'=>'required|numeric|max_digits:7|exists:gesestacas,estaca',
-            'detalleLev.*.estacaInim'=>'required_if:detalleLev.*.tipoLinea,O|numeric|max_digits:7|exclude_if:detalleLev.*.tipoLinea,A',
-            'detalleLev.*.estacaFin'=>'required_if:detalleLev.*.tipoLinea,R|numeric|max_digits:7|exists:gesestacas,estaca|gt:detalleLev.*.estacaIni|exclude_unless:detalleLev.*.tipoLinea,R',
-            'detalleLev.*.estacaFinm'=>'nullable|numeric|max_digits:7|min:1|exclude_unless:detalleLev.*.tipoLinea,R',
-            'detalleLev.*.cultivo'=>'required|integer|max_digits:7|exists:gescultivostab,idCultivo',
-        ];
-        $messages= [
-            //customMessages
-            //'proyecto.required' => 'El campo :attribute es requerido',
-            'detalleLev.*.linea.required'=>'la linea #:index es requerida'
-        ];
-        $attributes=[
-            //custom attributes
-            'idPermiso'=>'idPermiso',//se coloco para no separar el campo a= id permiso
-            //'detalleLev.*.tipoLinea' => 'tipo de línea en el detalle :position',
-            //'detalleLev.*.tipoLinea' => 'tipo de línea en el detalle :index',
-            //'detalleLev.*.tipoLinea' => 'tipo de línea en el detalle :position',
-            'detalleLev' => 'detalleLev',
-            'detalleLev.*.tipoLinea' => 'tipo de línea en el detalle',
-            'detalleLev.*.linea' => 'línea en el detalle',
-            'detalleLev.*.estacaIni' => 'estacaIni',
-            'detalleLev.*.estacaInim' => 'estacaInim',
-        ];
-        $validator = Validator::make($request->all(), $rules, $messages,$attributes);
-        //$request->validate($rules,$messages,$attributes);
+
+     /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreLevantamientoRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreLevantamientoRequest $request){
+       
+      //  $validator = Validator::make($request->all(), $rules, $messages,$attributes);
         //*agrupar los errores por cada elemento del arreglo 
-        if ($validator->fails()) { 
-            $errors = []; 
-            foreach ($validator->errors()->getMessages() as $field => $messages) { 
-                if (preg_match('/detalleLev\.(\d+)\./', $field, $matches)) { 
-                    $index = $matches[1]; 
-                    if (!isset($errors["detalleLev.$index"])) { 
-                    //if (!isset($errors['detalleLev'][$index])) {
-                        $errors["detalleLev.$index"] = []; 
-                    } 
-                    foreach ($messages as $message) { 
-                        $errors["detalleLev.$index"][] = $message; 
-                    } 
-                }
-                else { 
-                    $errors[$field] = $messages; 
-                } 
-            } 
-            return response()->json(['errors' => $errors], 422); 
-        }
+       
         
-        return  response()->json(["status"=>'todo ok','data'=>$validator->validated()],200);
+        //return  response()->json(["status"=>'todo ok','data'=>$validator->validated()],200);
+       /* $requValid=$request->validated();
+        $levantamiento = Levantamiento::create([ 
+            'fechaLevantamiento' => $requValid->fechaLev, 'observaciones' => $requValid->observaciones, 
+            'idPermiso' => $requValid->idPermiso,'idPersonal'=>57, 
+            'imgUrl' => '','numFiniquito'=>$requValid->finiquito, 
+        ]);*/
+        return  response()->json(["status"=>'todo ok',"data"=>$request->validated()],200);
 
     }
     
---
+++ app/Http/Requests/StoreLevantamientoRequest.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreLevantamientoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array{
+        return [
+            'fechaLev' => 'required|date_format:d/m/Y',
+            'observaciones' => 'nullable|string',
+            'idPermiso' => 'required|unique:geslevantamientos,idPermiso|integer|max_digits:5',
+            'idPersonal' => 'required|integer|exists:catpersonal,IdPersonal|max_digits:5',
+            //'imgUrl' => 'required|unique:geslevantamientos,idPermiso',
+            //'archivos',
+            'finiquito' => 'required|unique:geslevantamientos,numFiniquito|numeric|max_digits:5',
+            //TODO agregar los campos del detalle levantamiento
+            'detalleLev'=>'required|array',
+            'detalleLev.*.tipoLinea'=>'required|string|max:2|in:R,A,O',
+            'detalleLev.*.linea'=>'required|integer|max_digits:10|exists:gesconfiguracionlineas,linea',
+            'detalleLev.*.estacaIni'=>'required|numeric|max_digits:7|exists:gesestacas,estaca',
+            'detalleLev.*.estacaInim'=>'required_if:detalleLev.*.tipoLinea,O|numeric|max_digits:7|exclude_if:detalleLev.*.tipoLinea,A',
+            'detalleLev.*.estacaFin'=>'required_if:detalleLev.*.tipoLinea,R|numeric|max_digits:7|exists:gesestacas,estaca|gt:detalleLev.*.estacaIni|exclude_unless:detalleLev.*.tipoLinea,R',
+            'detalleLev.*.estacaFinm'=>'nullable|numeric|max_digits:7|min:1|exclude_unless:detalleLev.*.tipoLinea,R',
+            'detalleLev.*.cultivo'=>'required|integer|max_digits:7|exists:gescultivostab,idCultivo',
+            'detalleLev.*.metros'=>'numeric|max_digits:7|min:0',
+            'detalleLev.*.metros2'=>'numeric|max_digits:7|min:0',
+            'detalleLev.*.km'=>'numeric|max_digits:7|min:0',
+            'detalleLev.*.ha'=>'numeric|max_digits:7|min:0|min_digits:1|required',
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array{
+        return [
+        //customMessages
+            //'proyecto.required' => 'El campo :attribute es requerido',
+            'detalleLev.*.linea.required'=>'la linea #:index es requerida'
+        ];
+    }
+
+    /**
+     * Get custom attributes for validator errors.
+     *
+     * @return array<string, string>
+     */
+    public function attributes(): array{
+        return [
+            //custom attributes
+            'idPermiso'=>'idPermiso',//se coloco para no separar el campo a= id permiso
+            //'detalleLev.*.tipoLinea' => 'tipo de línea en el detalle :position',
+            //'detalleLev.*.tipoLinea' => 'tipo de línea en el detalle :index',
+            //'detalleLev.*.tipoLinea' => 'tipo de línea en el detalle :position',
+            'detalleLev' => 'detalleLev',
+            'detalleLev.*.tipoLinea' => 'tipo de línea en el detalle',
+            'detalleLev.*.linea' => 'línea en el detalle',
+            'detalleLev.*.estacaIni' => 'estacaIni',
+            'detalleLev.*.estacaInim' => 'estacaInim',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator){
+       /* throw new HttpResponseException(response()->json([
+            'erroras' => $validator->errors()->first(),
+        ], 572));*/
+        //* agrupar los errores por cada elemento del arreglo 
+        if ($validator->fails()) { 
+            $errors = []; 
+            foreach ($validator->errors()->getMessages() as $field => $messages) { 
+                if (preg_match('/detalleLev\.(\d+)\./', $field, $matches)) { 
+                    $index = $matches[1]; 
+                    if (!isset($errors["detalleLev.$index"])) { 
+                    //if (!isset($errors['detalleLev'][$index])) {
+                        $errors["detalleLev.$index"] = []; 
+                    } 
+                    foreach ($messages as $message) { 
+                        $errors["detalleLev.$index"][] = $message; 
+                    } 
+                }
+                else { 
+                    $errors[$field] = $messages; 
+                } 
+            } 
+           // return response()->json(['errors' => $errors], 422); 
+           
+           throw new HttpResponseException(response()->json([
+            'errors' => $errors], 572));
+        }
+    }
+}
